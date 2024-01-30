c46d6dca5456215017cc0fe31342d13f944b0b89
MaryelN <maryel.navarromtz@gmail.com>
2024-01-30 23:20:29 +0100

    add of ReviewType form into the movie details

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Movie;
+use App\Entity\Review;
+use App\Form\ReviewType;
 use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,8 +25,12 @@ class MovieController extends AbstractController
     #[Route('/movie/{id}', name: 'app_movie_detail')]
     public function detail(Movie $movie):Response
     {
+        $review = new Review();
+        $form =$this->createForm(ReviewType::class, $review);
+
         return $this->render('movie/detail.html.twig', [
             'movie' => $movie,
+            'form' => $form->createView(),
         ]);
     }
 }
